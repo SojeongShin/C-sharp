18719233733fbc5ac9998f7c1b3f69216fa05ca9
Shin SoJeong <86238406+SojeongShin@users.noreply.github.com>
2021-11-03 13:08:53 +0900

    midtermExam

---
+++ midtermExam/문제1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ssj581
+{
+    class 문제1 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("택시로 이동 할 거리(KM)를 입력하세요 : ");
+            int distance =Convert.ToInt32(Console.ReadLine());
+
+            Taxi t = new Taxi(distance);
+            t.Distance = distance;
+            Taxi.Taxi(t.Distance);
+        }
+    }
+
+    public class Taxi : iCalc
+    {
+        private int _Distance;  // 이동거리
+
+        public Taxi(int distance) 
+        {
+            Console.WriteLine("이동거리 {0} KM는 {1}이고", distance, MileTransform());
+            Console.WriteLine("택시요금은 {0}원 이다.", Money());
+        }
+
+        public int Distance
+        {
+            get { return _Distance; }
+            set { _Distance = value; }
+        }
+
+        public int Money()
+        {
+            return 2000 * _Distance / 100 * 110;  // 부가세 10퍼센트 
+
+        }
+
+        public double MileTransform()
+        {
+            return _Distance / 8 * 5;  // 1마일 = 1.6키로 
+        
+        }
+    }
+
+    public interface iCalc
+    {
+        public int Money();
+        double MileTransform();
+    }
+}
---
+++ midtermExam/문제2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ssj581
+{
+    public class 문제2
+    {
+        public static void Main(string[] args)
+        {
+            int remain;
+            Console.Write("물건의 가격을 입력하세요(1000원 미만) : ");
+            int won = Convert.ToInt32(Console.ReadLine());
+
+            int total = 1000 - won;  // 거스름돈 총액
+
+            int obeak = total / 500;  // 오백원짜리 갯수
+            remain = total - obeak * 500;
+
+            int beak = remain / 100;  // 백원짜리 갯수
+            remain -= beak * 100;
+
+            int osip = remain / 50;  // 오십원짜리 갯수
+            remain -= osip * 50;  
+
+            int sip = remain / 10;  // 십원짜리 갯수 
+            remain -= sip * 10;
+
+            int il = remain;
+
+            Console.WriteLine("거스름돈 입니다.\n");
+            Console.WriteLine("500원 {0} 개", obeak);
+            Console.WriteLine("100원 {0} 개", beak);
+            Console.WriteLine("50원 {0} 개", osip);
+            Console.WriteLine("10원 {0} 개", sip);
+            Console.WriteLine("1원 {0} 개", il);
+            Console.WriteLine("거스름돈은 {0} 원 입니다.", total);
+        }
+    }
+}
